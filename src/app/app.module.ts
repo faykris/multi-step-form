81e17482d808ee71e59c210eab087475fddeb89c
faykris <faykris28@gmail.com>
2023-09-22 22:16:38 -0500

    Add Personal Info form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { MultiStepFormComponent } from './multi-step-form/multi-step-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { MultiStepFormComponent } from './multi-step-form/multi-step-form.compon
     MultiStepFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/multi-step-form/multi-step-form.component.ts
+++ src/app/multi-step-form/multi-step-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-step-form',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./multi-step-form.component.css']
 })
 export class MultiStepFormComponent {
+  addUserForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.addUserForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]]
+    });
+  }
+
+  onSubmit() {
+
+  }
 
 }
